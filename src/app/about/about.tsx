f9d26be551d80072a060541484c1d29d06965b32
Soumil Singh <soumil.sps@gmail.com>
2024-08-18 01:02:39 +0530

    fixed resume button

--- src/app/about/about.tsx
+++ src/app/about/about.tsx
@@ -1,33 +1,46 @@
-'use client';
+"use client";
 import ArrowBtn from "../components/arrow-btn/arrow-btn";
+import { FilePdf } from "phosphor-react";
 export default function About() {
-
   const openResume = () => {
     // resume in pdf format in public folder
-    window.open('/Resume_Soumil Singh.pdf', '_blank');
-  }
+    window.open("/Resume_Soumil Singh.pdf", "_blank");
+  };
 
   return (
     <section id="about" data-target="about" className="about">
       <div className="about-container">
         <div className="about-heading">
           <h1>About Me</h1>
-          <h5>Get to know me</h5>
+          <h5>A brief introduction</h5>
         </div>
         <div className="about-content">
           <div className="about-text">
-            <h2>Hi, I'm John Doe</h2>
+            <h2>Hi, I'm Soumil Singh</h2>
             <p>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam
-              euismod, nisl nec lacinia posuere, justo nunc bibendum tellus, vel
-              fermentum nulla libero sit amet nulla. Sed at nunc fermentum,
-              ultrices odio nec, sollicitudin odio. Nullam vestibulum, elit nec
-              ultricies ultricies, urna orci vehicula eros, nec lacinia purus
-              elit nec felis. Nulla facilisi.
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed
+              tincidunt, augue nec ultricies ultricies, libero libero facilisis
+              purus, nec ultricies libero libero nec. Sed tincidunt, augue nec
+              ultricies ultricies, libero libero facilisis purus, nec ultricies
+              libero libero nec.
+              <br />
+              <br/>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed
+              tincidunt, augue nec ultricies ultricies, libero libero facilisis
+              purus, nec ultricies libero libero nec. Sed tincidunt, augue nec
+              ultricies ultricies, libero libero facilisis purus, nec ultricies
+              libero libero nec.
+              <br />
+              <br/>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed
+              tincidunt, augue nec ultricies ultricies, libero libero facilisis
+              purus, nec ultricies libero libero nec. Sed tincidunt, augue nec
+              ultricies ultricies, libero libero facilisis purus, nec ultricies
+              libero libero nec.
+              <br />
             </p>
-            <div className="about-buttons">
-              {/* <button className="resume-btn" onClick={openResume}>Resume</button> */}
-              <ArrowBtn name="Resume" onClick={openResume}/>
+            <div className="resume-btn">
+              <ArrowBtn name="Resume" onClick={openResume} icon={<FilePdf />} />
             </div>
           </div>
         </div>
